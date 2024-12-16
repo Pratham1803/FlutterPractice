12885e5f567c310be3606da2ca08f8d5c15b359b
pratham <prathamrathod1812@gmail.com>
2024-12-17 04:52:14 +0530

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:first_flutter_app/text_style.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.blue,
+            Colors.green,
+            Colors.yellow,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:first_flutter_app/gradient_container.dart';
+void main() {
+  // basic example of Background color
+  // runApp(
+  //   const MaterialApp(
+  //     home: Scaffold(
+  //       backgroundColor: Colors.lightBlue,
+  //       body: Center(
+  //         child: Text('Hello World'),
+  //       ),
+  //     ),
+  //   ),
+  // );
+
+  // Set Gradient backgrond and text style Example
+  // runApp(
+  //   MaterialApp(
+  //     home: Scaffold(
+  //       body: Container(
+  //         decoration: const BoxDecoration(
+  //           gradient: LinearGradient(
+  //             colors: [
+  //               Colors.blue,
+  //               Colors.green,
+  //               Colors.yellow,
+  //             ],
+  //             begin: Alignment.topLeft,
+  //             end: Alignment.bottomRight,
+  //           ),
+  //         ),
+  //         child: const Center(
+  //           child: Text(
+  //             'Hello World',
+  //             style: TextStyle(
+  //               color: Colors.white,
+  //               fontSize: 36,
+  //               fontWeight: FontWeight.bold,
+  //             ),
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   ),
+  // );
+
+  // Custom Widget Example
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  ); 
+}
