c124a5ee18de298478d61a40ef8b7c40c7146df6
pratham <prathamrathod1812@gmail.com>
2025-02-08 16:26:22 +0530

    Add meal and category models, implement category selection and meal display

---
+++ meals/lib/screens/meals.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:meals/model/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(itemCount: meals.length,itemBuilder: (context, index) {
+      // return ListTile(
+      //   title: Text(meals[index].title),
+      // );
+      return MealItem(meal: meals[index]);
+    });
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Uh oh! No meals found!",
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              "Try selecting Different Categories...",
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content);
+  }
+}
